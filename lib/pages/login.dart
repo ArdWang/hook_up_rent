fe8696ebb4b72fc0a196769db4ff00f293e3b65e
Jing Wang <278161009@qq.com>
2020-06-30 19:34:07 +0800

    update rnd

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:hook_up_rent/application.dart';
-import 'package:hook_up_rent/widgets/page_content.dart';
-
+//import 'package:hook_up_rent/widgets/page_content.dart';
 
 class LoginPage extends StatefulWidget {
-
   LoginPage();
 
   @override
@@ -12,60 +10,73 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-
   bool showPassword = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          '登录'
-        ),
+        title: Text('登录'),
       ),
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            TextField(
-              decoration: InputDecoration(
-                labelText: '用户名',
-                hintText: '请输入用户名'
+      body: SafeArea(
+        minimum: EdgeInsets.all(30.0),
+        child: Container(
+          child: Column(
+            children: <Widget>[
+              TextField(
+                decoration:
+                    InputDecoration(labelText: '用户名', hintText: '请输入用户名'),
               ),
-            ),
-            TextField(
-              decoration: InputDecoration(
-                labelText: '密码',
-                hintText: '请输入密码',
-                suffixIcon: IconButton(
-                  icon:Icon(showPassword? Icons.visibility_off: Icons.visibility),
-                  onPressed: (){
-                    setState(() {
-                      showPassword = !showPassword;
-                    });
-                  },
-                ),
+              Padding(
+                padding: EdgeInsets.all(10),
               ),
-              obscureText: !showPassword,
-            ),
-            RaisedButton(
-              child: Text('登录'),
-              onPressed: (){
-                //TODO 登录按钮事件
-              },
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text('还没有账号,'),
-                FlatButton(
-                  onPressed: (){
-                    Application.router.navigateTo(context, '/register');
-                  },
-                  child: Text('去注册~')
+              TextField(
+                decoration: InputDecoration(
+                  labelText: '密码',
+                  hintText: '请输入密码',
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                        showPassword ? Icons.visibility_off : Icons.visibility),
+                    onPressed: () {
+                      setState(() {
+                        showPassword = !showPassword;
+                      });
+                    },
+                  ),
                 ),
-              ],
-            )
-          ],
+                obscureText: !showPassword,
+              ),
+              Padding(
+                padding: EdgeInsets.all(10),
+              ),
+              RaisedButton(
+                color: Colors.green,
+                child: Text(
+                  '登录',
+                  style: TextStyle(
+                    color: Colors.white
+                  ),
+                ),
+                onPressed: () {
+                  //TODO 登录按钮事件
+                },
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text('还没有账号,'),
+                  FlatButton(
+                      onPressed: () {
+                        Application.router.navigateTo(context, '/register');
+                      },
+                      child: Text(
+                        '去注册~',
+                        style: TextStyle(color: Colors.green),
+                      )),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
